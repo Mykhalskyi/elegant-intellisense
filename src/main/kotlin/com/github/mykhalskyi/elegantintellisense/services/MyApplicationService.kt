90bb4614e839965f35c539b79ed8c39705e70fcf
GitHub Action <action@github.com>
2022-06-01 20:43:55 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mykhalskyi/elegantintellisense/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mykhalskyi.elegantintellisense.services
+
+import com.github.mykhalskyi.elegantintellisense.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
